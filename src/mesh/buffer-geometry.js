d9e0157ed852b0cf4ef26b5d90452cb5ff6e8459
afpaiva <andrepaiva.arq@gmail.com>
2024-09-25 17:11:08 -0300

    2 faces

--- src/mesh/buffer-geometry.js
+++ src/mesh/buffer-geometry.js
@@ -9,25 +9,41 @@ export const bufferGeometry = () => {// Basic setup of the scene with Three.js
     document.body.appendChild(renderer.domElement);
 
     // Create a triangle
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const material = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true,
+    });
     material.side = THREE.DoubleSide
 
 
     // creating a BufferGeometry
-    const positionArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+    const positionArray = new Float32Array([
+        -1, 0, 0, // vertex 1
+        0, 1, 0,  // vertex 2
+        1, 0, 0,  // vertex 3
+        1, 1, 1   // vertex 4
+    ]);
     const positionAttribute = new THREE.BufferAttribute(positionArray, 3);
-    const triangleGeometry = new THREE.BufferGeometry();
-    triangleGeometry.setAttribute('position', positionAttribute);
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', positionAttribute);
 
-    const triangle = new THREE.Mesh(triangleGeometry, material)
+    // add indexes, so the 4 coordinates will generate 2 triangles
+    // sharing exactly the same vertexes
+    const indexArray = new Uint16Array([
+        0, 1, 2, // first triangle is made by vertexes of index 0, 1 and 2
+        1, 3, 2  // second triangle is made by vertexes of index 1, 3 and 2
+    ])
+    const indexAttributes = new THREE.BufferAttribute(indexArray, 1);
+    geometry.setIndex(indexAttributes);
 
-    scene.add(triangle);
+    const mesh = new THREE.Mesh(geometry, material)
+    scene.add(mesh);
 
     camera.position.z = 5;
 
     // Use GSAP to animate the triangle (no loop for GSAP animation)
-    gsap.to(triangle.rotation, { duration: 2, x: Math.PI * 2, repeat: -1, ease: "linear" });
-    gsap.to(triangle.rotation, { duration: 2, z: Math.PI * 2, repeat: -1, ease: "linear" });
+    gsap.to(mesh.rotation, { duration: 2, x: Math.PI * 2, repeat: -1, ease: "linear" });
+    gsap.to(mesh.rotation, { duration: 2, z: Math.PI * 2, repeat: -1, ease: "linear" });
 
     // Rendering loop for the scene
     function animate() {
